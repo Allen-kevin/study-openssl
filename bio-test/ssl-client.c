322cb058bb330cb57f2d42c53cf79379b8d7d6c9
ATCP <poppin.ke@gmail.com>
2020-10-23 04:32:35 +0800

    Add a bio example

---
+++ bio-test/ssl-client.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define MAXBUF 1024
+
+void ShowCerts(SSL *ssl)
+{
+	X509 *cert;
+	char *line;
+
+	cert = SSL_get_peer_certificate(ssl);
+	if (cert != NULL) {
+		line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+		free(line);
+		line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+		free(line);
+		X509_free(cert);
+	} else {
+		printf("No msg about certificate.\n");
+	}
+}
+
+
+int tcp_socket(char *str, int len, char **argv)
+{
+    int sockfd;
+    struct sockaddr_in dest;
+
+    bzero(&dest, sizeof(dest));
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(atoi(argv[2]));
+    if (inet_aton(argv[1], (struct in_addr *)&dest.sin_addr.s_addr) == 0) {
+        perror(argv[1]);
+        exit(errno);
+    }
+    
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket\n");
+        return -1;
+    }
+
+    printf("address created\n");
+    if (connect(sockfd, (struct sockaddr *)&dest, sizeof(dest)) != 0) {
+        perror("Connect ");
+        exit(errno);
+    }
+    printf("Server connected\n");
+
+    return sockfd;
+}
+
+
+void tls_init(SSL_CTX *ctx, SSL *ssl, BIO *client_io)
+{
+  
+}
+
+
+int main(int argc, char **argv)
+{	
+    if (argc != 3) {
+		printf("parameter error!\n");
+		exit(0);
+	}
+
+	int len, sockfd, ret = 5;
+	char buffer[MAXBUF+1];
+	SSL_CTX *ctx;
+	SSL *ssl;
+
+	SSL_library_init();
+	OpenSSL_add_all_algorithms();
+	SSL_load_error_strings();
+	ctx = SSL_CTX_new(SSLv23_client_method());
+	if (ctx == NULL) {
+		ERR_print_errors_fp(stdout);
+		exit(1);
+	}
+    
+    BIO *client = NULL, *client_io = NULL;
+
+    size_t bufsiz = 256;
+
+    if (!BIO_new_bio_pair(&client, bufsiz, &client_io, bufsiz))
+        goto err;
+
+	ssl = SSL_new(ctx);
+    SSL_set_bio(ssl, client, client);
+
+    ret = SSL_connect(ssl);
+    printf("ret = %d\n", ret);
+
+	len = BIO_read(client_io, buffer, MAXBUF);
+	if (len > 0) {
+		printf("rcv msg success: %s, total %d bytes\n", buffer, len);
+	} else {
+		printf("rcv msg failure!, error code %d, error msg %s\n",
+			errno, strerror(errno));
+		goto err;
+	}
+    
+
+err:
+	SSL_shutdown(ssl);
+	SSL_free(ssl);
+	SSL_CTX_free(ctx);
+	return 0;
+}
